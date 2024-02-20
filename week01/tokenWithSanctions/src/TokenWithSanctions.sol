db581c37597f6778898591d184b610f13521fb47
andrejrakic <andrej.rakic@smartcontract.com>
2024-02-20 10:19:38 +0100

    feat: Add TokenWithSanctions project

---
+++ week01/tokenWithSanctions/src/TokenWithSanctions.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "./vendor/openzeppelin/contracts/v5.0.0/token/ERC20/ERC20.sol";
+import {Ownable} from "./utils/Ownable.sol";
+
+contract TokenWithSanctions is ERC20, Ownable {
+    mapping(address tokenHolder => bool isBanned) internal s_banned;
+
+    event Banned(address indexed tokenHolder);
+
+    error TokenWithSanctions__BannedFromSending(address);
+    error TokenWithSanctions__BannedFromReceiving(address);
+
+    constructor(
+        address _owner,
+        address _pendingOwner
+    ) ERC20("MyToken", "MTK") Ownable(_owner, _pendingOwner) {}
+
+    function mint(address _to, uint256 _amount) external onlyOwner {
+        _mint(_to, _amount);
+    }
+
+    function ban(address _tokenHolder) external onlyOwner {
+        s_banned[_tokenHolder] = true;
+
+        emit Banned(_tokenHolder);
+    }
+
+    function isBanned(address _tokenHolder) public view returns (bool) {
+        return s_banned[_tokenHolder];
+    }
+
+    function _update(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        if (isBanned(from)) revert TokenWithSanctions__BannedFromSending(from);
+        if (isBanned(to)) revert TokenWithSanctions__BannedFromReceiving(to);
+
+        super._update(from, to, amount);
+    }
+}
---
+++ week01/tokenWithSanctions/test/TokenWithSanctions.t.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {TokenWithSanctions} from "./../src/TokenWithSanctions.sol";
+
+contract TokenWithSanctionsTest is Test {
+    TokenWithSanctions public tokenWithSanctions;
+    address owner;
+    address alice;
+    address bob;
+
+    function setUp() public {
+        owner = makeAddr("owner");
+        alice = makeAddr("alice");
+        bob = makeAddr("bob");
+
+        tokenWithSanctions = new TokenWithSanctions(owner, address(0));
+    }
+
+    function test_Soak() public {
+        vm.startPrank(owner);
+        uint256 amountToMint = 100;
+        tokenWithSanctions.mint(alice, amountToMint);
+        tokenWithSanctions.mint(bob, amountToMint);
+
+        assertEq(tokenWithSanctions.balanceOf(alice), amountToMint);
+        assertEq(tokenWithSanctions.balanceOf(bob), amountToMint);
+        assertEq(tokenWithSanctions.totalSupply(), 2 * amountToMint);
+        vm.stopPrank();
+
+        vm.startPrank(alice);
+        uint256 amountToTransfer = 20;
+        tokenWithSanctions.transfer(bob, amountToTransfer);
+        assertEq(
+            tokenWithSanctions.balanceOf(alice),
+            amountToMint - amountToTransfer
+        );
+        assertEq(
+            tokenWithSanctions.balanceOf(bob),
+            amountToMint + amountToTransfer
+        );
+        vm.stopPrank();
+
+        vm.startPrank(owner);
+        tokenWithSanctions.ban(alice);
+        assertEq(tokenWithSanctions.isBanned(alice), true);
+        vm.stopPrank();
+
+        vm.startPrank(alice);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                TokenWithSanctions
+                    .TokenWithSanctions__BannedFromSending
+                    .selector,
+                alice
+            )
+        );
+        tokenWithSanctions.transfer(bob, amountToTransfer);
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                TokenWithSanctions
+                    .TokenWithSanctions__BannedFromReceiving
+                    .selector,
+                alice
+            )
+        );
+        tokenWithSanctions.transfer(alice, amountToTransfer);
+        vm.stopPrank();
+
+        // sanity check
+        assertEq(
+            tokenWithSanctions.balanceOf(alice),
+            amountToMint - amountToTransfer
+        );
+        assertEq(
+            tokenWithSanctions.balanceOf(bob),
+            amountToMint + amountToTransfer
+        );
+    }
+}
